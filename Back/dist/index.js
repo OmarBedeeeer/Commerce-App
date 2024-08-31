0af3cb1b0c88075b45377235bfbae4df5c1b4810
bedeer <calabit_2@yahoo.com>
2024-08-31 19:02:14 +0300

    Creating The Back-end

---
+++ Back/dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const app = (0, express_1.default)();
+dotenv_1.default.config();
+app.use(express_1.default.json());
+const port = process.env.PORT;
+app.listen(port, () => console.log("App listening on port"));
