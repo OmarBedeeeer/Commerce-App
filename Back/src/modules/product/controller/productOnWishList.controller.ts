08aef592daece4da6ee9437aa9de13f8e1221190
omar bedeer <omarbedeer5@gmail.com>
2024-09-12 17:32:45 +0300

    create a wish list for Users

--- Back/src/modules/product/controller/productOnWishList.controller.ts
+++ Back/src/modules/product/controller/productOnWishList.controller.ts
@@ -3,33 +3,42 @@ import { Request, Response } from "express";
 import { CatchError, AppError } from "../../../utils/errorhandler";
 import Product from "../model/product.model";
 import { IUser } from "../../../interfaces/dbinterfaces";
+import { ParamsIds } from "../../../interfaces/Queryinterfaces";
 
 export const productOnWishList = {
-  addProdToWishList: CatchError(async (req: Request, res: Response) => {
-    const { userId } = req.params;
-    const { productId } = req.body;
+  addProdToWishList: CatchError(
+    async (req: Request<ParamsIds>, res: Response) => {
+      const { productId } = req.params;
 
-    const user: IUser | null = await User.findByIdAndUpdate(
-      req.user!.id,
-      {
-        $addToSet: { wishList: productId },
-      },
-      {
-        new: true,
-      }
-    );
+      const user: IUser | null = await User.findByIdAndUpdate(
+        req.user!.id,
+        {
+          $addToSet: { wishList: productId },
+        },
+        {
+          new: true,
+        }
+      );
 
-    if (!user) throw new AppError("Please Login first", 404);
+      if (!user) throw new AppError("Please Login first", 404);
 
-    res.status(200).json({
-      message: "Product added to wishlist",
-      success: true,
-      user,
-    });
-  }),
+      res.status(200).json({
+        message: "Product added to wishlist",
+        success: true,
+        user,
+      });
+    }
+  ),
 
   getWishList: CatchError(async (req: Request, res: Response) => {
-    const user: IUser | null = await User.findById({ _id: req.user?.id });
+    const user: IUser | null = await User.findById({
+      _id: req.user!.id,
+    })
+      .select("username wishList")
+      .populate({
+        path: "wishList",
+        select: "name price image",
+      });
     if (!user) throw new AppError("User not found", 404);
 
     res.status(200).json({
@@ -38,26 +47,27 @@ export const productOnWishList = {
       user,
     });
   }),
-  removeFromWishList: CatchError(async (req: Request, res: Response) => {
-    const { userId } = req.params;
-    const { productId } = req.body;
+  removeFromWishList: CatchError(
+    async (req: Request<ParamsIds>, res: Response) => {
+      const { productId } = req.params;
 
-    const user: IUser | null = await User.findByIdAndUpdate(
-      req.user?.id,
-      {
-        $pull: { wishList: productId },
-      },
-      {
-        new: true,
-      }
-    );
+      const user: IUser | null = await User.findByIdAndUpdate(
+        req.user!.id,
+        {
+          $pull: { wishList: productId },
+        },
+        {
+          new: true,
+        }
+      );
 
-    if (!user) throw new AppError("Please Login first", 404);
+      if (!user) throw new AppError("Please Login first", 404);
 
-    res.status(200).json({
-      message: "Product removed from wishlist",
-      success: true,
-      user,
-    });
-  }),
+      res.status(200).json({
+        message: "Product removed from wishlist",
+        success: true,
+        user,
+      });
+    }
+  ),
 };
